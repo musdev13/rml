bbc44599a33627580f2b4cdec8f1e9e39bf6a4b6
MusDev13 <sonics0977703996@gmail.com>
2026-07-13 18:40:25 +0300

    init command done?

--- src/core/config/init.rs
+++ src/core/config/init.rs
@@ -1,4 +1,22 @@
-pub fn init(force: bool) {
+use serde_json::{json, Value};
+use std::sync::LazyLock;
+
+pub static DEFAULT_PATHS_CONFIG: LazyLock<Value> = LazyLock::new(|| {
+    json!({
+        "verisons_path": "~/rml/versions",
+        "assets_path": "~/rml/assets",
+        "libs_path": "~/rml/libs"
+    })
+});
+
+pub fn init(force: bool){
     println!("initializing...");
-    if force {println!("forced");}
+    let def_config_str = serde_json::to_string_pretty(&*DEFAULT_PATHS_CONFIG).unwrap();
+    if force {
+        println!("!!! FORCED !!!");
+        musutils::fs::config::rewrite("rmlaunch", "paths.json", &def_config_str);
+    } else {
+        musutils::fs::config::get("rmlaunch", "paths.json", Some(&def_config_str));
+    }
+    println!("done :3\n\n!!! check your ~/.config or %AppData% directory for rmlaunch configuration !!!");
 }
