473cd41b11e132b1b618f105f12a59bd663c5925
MusDev13 <sonics0977703996@gmail.com>
2026-07-13 22:58:17 +0300

    renamed to rml

--- src/core/config/init.rs
+++ src/core/config/init.rs
@@ -14,9 +14,9 @@ pub fn init(force: bool){
     let def_config_str = serde_json::to_string_pretty(&*DEFAULT_PATHS_CONFIG).unwrap();
     if force {
         println!("!!! FORCED !!!");
-        musutils::fs::config::rewrite("rmlaunch", "paths.json", &def_config_str);
+        musutils::fs::config::rewrite("rml", "paths.json", &def_config_str);
     } else {
-        musutils::fs::config::get("rmlaunch", "paths.json", Some(&def_config_str));
+        musutils::fs::config::get("rml", "paths.json", Some(&def_config_str));
     }
-    println!("done :3\n\n!!! check your ~/.config or %AppData% directory for rmlaunch configuration !!!");
+    println!("done :3\n\n!!! check your ~/.config or %AppData% directory for rml configuration !!!");
 }
