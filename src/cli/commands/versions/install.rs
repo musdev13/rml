6e7d1518b76200c61bb6f94cd8f7226e60ee1b4d
MusDev13 <sonics0977703996@gmail.com>
2026-07-15 22:33:32 +0300

    libs path option

--- src/cli/commands/versions/install.rs
+++ src/cli/commands/versions/install.rs
@@ -8,9 +8,11 @@ pub struct InstallArgs {
     pub version_id: String,
     #[arg(short, long, value_name = "PATH")]
     pub directory: Option<PathBuf>,
+    #[arg(short, long, value_name = "LIBS_PATH")]
+    pub libs: Option<PathBuf>,
 }
 
-pub async fn handle(version_id: String, directory: Option<PathBuf>) {
-    println!("{}, {}", version_id, directory.clone().map_or("default".to_string(), |p| p.display().to_string()));
-    core::version::install(version_id, directory).await;
+pub async fn handle(version_id: String, directory: Option<PathBuf>, libs: Option<PathBuf>) {
+    println!("{}\n{}\n{}\n{}", version_id, "-".repeat(10), directory.clone().map_or("default".to_string(), |p| p.display().to_string()), libs.clone().map_or("default".to_string(), |p| p.display().to_string()));
+    core::version::install(version_id, directory, libs).await;
 }
--- src/core/version/install.rs
+++ src/core/version/install.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use crate::core;
 
-pub async fn install(version_id: String, directory: Option<PathBuf>){
+pub async fn install(version_id: String, versions_path_option: Option<PathBuf>, libs_path_option: Option<PathBuf>){
     let version_json_str = core::version::get_version_json(version_id).await;
     println!("{}", version_json_str);
 
